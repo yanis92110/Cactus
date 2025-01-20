264797959606cbb506db1ca47bf03120596db256
yanis92110 <yanislaidd@gmail.com>
2025-01-20 10:30:00 +0100

    Affichage cartes par 4

--- interface.py
+++ interface.py
@@ -13,41 +13,51 @@ window = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Jeu de Cartes")
 
 images_path_joueur=[]
-print(len(mains_joueurs))
-print(mains_joueurs[0])
+
 
 
 for main in mains_joueurs:
     for i in range(0,4):
         images_path_joueur.append((f"static/img/{main.cartes[i]}"))
-print(images_path_joueur)
+
 
 
 loaded_images = []
 
 for path in images_path_joueur:
     image = pygame.image.load(f"{path}.png")
-    image = pygame.transform.scale(image,(50,100))
+    image = pygame.transform.scale(image,(100,150))
     loaded_images.append(image)
 image_positions=[]
-x=-100
+x=250
 y=0
+i=0
+print(i%2)
 for position in loaded_images:
+    if i%1:
+        y+=200
+        x=250
+    if i>=4:
+        y=150
+        x=-100
+    x+=100
+    i+=1
+    image_positions.append((x,y))
+"""for position in loaded_images:
+
     if(x>200):
         x=-100
         y+=300
-    x+=200
+    x+=100
     image_positions.append((x,y))
-
-
-
+"""
 # Boucle principale
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.mouse.get_focused:
+        if event.type == pygame.mouse.get_pressed:
             print("Appuyé !")
 
     # Remplir l'arrière-plan
@@ -58,8 +68,12 @@ while running:
      #   window.blit(image, image_positions[i])
 
     #Affiche les cartes cachées a la position voulue
+    scaled_image=pygame.image.load("static/img/base.jpg")
+    scaled_image=pygame.transform.scale(scaled_image,(100,150))
     for i, image in enumerate(loaded_images):
-        window.blit(pygame.image.load("static/img/base.jpg"),image_positions[i])
+        
+
+        window.blit(scaled_image,image_positions[i])
     window.blit(pygame.image.load("static/img/base.jpg"),(900,0))
     # Mettre à jour l'affichage
     pygame.display.flip()
--- jeu.py
+++ jeu.py
@@ -3,14 +3,15 @@ from fonctions import *
 #Debut du jeu
 
 ### Tests
+"""
 print("Entrer le nombre de joueurs:")
 nbjoueurs=input()
 
 while not(nbjoueurs.isdigit() and 2<= int(nbjoueurs)<=10):
     print("Entrer le nombre de joueurs (entre 2 et 10):")
     nbjoueurs=input()
-
-nbjoueurs=int(nbjoueurs)
+"""
+nbjoueurs=2#int(nbjoueurs)
 tour=0
 #global mains_joueurs
 mains_joueurs=[Main(f'Joueur {i+1}') for i in range(nbjoueurs)]
@@ -19,9 +20,7 @@ for _ in range(4):
     for main in mains_joueurs:
         if paquet.cartes:
             main.ajouter_carte(paquet.pop_carte())
-print("\nMains des joueurs après distribution :")
-for main in mains_joueurs:
-    montrerCarte(main)
+
 finjeu=False
 
 ### Debut du jeu
@@ -54,13 +53,13 @@ while(finjeu):
         finjeu=True
     
 #Quelqu'un a fait cactus
-print("Fin du jeu ! Calcul des points...")
+#print("Fin du jeu ! Calcul des points...")
 time.sleep(1)
 points_joueurs=[]
 for joueur in range(nbjoueurs):
     for cartes in mains_joueurs[joueur].cartes:
         points_joueurs.append(0)
         points_joueurs[joueur] += cartes.points
-print("Total des points:\n")
-for joueur in range(nbjoueurs):
-    print(f"Joueur {joueur+1}: {points_joueurs[joueur]} points !")
+#print("Total des points:\n")
+#for joueur in range(nbjoueurs):
+#    print(f"Joueur {joueur+1}: {points_joueurs[joueur]} points !")
